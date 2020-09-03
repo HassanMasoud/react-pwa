10f40dd0ae3d907daea23733c700273a90513f7a
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-03 19:40:07 +0300

    Listen for requests

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -4,12 +4,21 @@ const urlsToCache = ["index.html", "offline.html"];
 const self = this;
 
 // Install SW
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
 
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (e) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
     })
   );
 });
